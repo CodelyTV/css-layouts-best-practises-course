7c4d75c8ddf1919e347ba377d6ad5022c57263d9
Nuria <soriano.nuria@gmail.com>
2020-09-21 09:46:44 +0200

    add lesson 4-1-modal-position start

---
+++ pages/4-1-modal-position/components/CourseInfo.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="course-content">
+    <h2 class="course-content-title">Domain-Driven Design - DDD Aplicado</h2>
+    <p class="course-content-lead">
+      Aprende a modelar tus aplicaciones centrándote en tu dominio, definir
+      Bounded Contexts, agregados, value objects, y mucho más. Todo de forma
+      práctica y con ejemplos de código ⚡
+    </p>
+
+    <h3>💸 ¡Primera lección disponible sin registro!</h3>
+    <p>
+      En el curso de Event-Driven Architecture analizamos cómo se comunican los
+      distintos servicios de nuestra empresa, pero… ¿Cómo modelamos los
+      servicios y aplicaciones de nuestra empresa?
+    </p>
+
+    <p>
+      Si queremos modelar un sistema complejo necesitamos complementar
+      principios y técnicas como los principios SOLID o la Arquitectura
+      Hexagonal con una parte estratégica. Es ahí donde entra Domain-Driven
+      Design (DDD).
+    </p>
+
+    <p>
+      DDD hace de paraguas agrupando toda una serie de conceptos a nivel técnico
+      y estratégico para promover:
+    </p>
+
+    <ul>
+      <li>
+        Colaboración entre negocio o producto y el equipo técnico para modelar
+        en código los problemas particulares de un determinado dominio (Bounded
+        Contexts, Ubiquitous Language, Agregados, Value Objects, Eventos de
+        Dominio, Entidades, Repositorios…)
+      </li>
+      <li>
+        Poner el foco de nuestra aplicación en el dominio. Manteniendo una alta
+        tolerancia al cambio y evolución de los sistemas a lo largo del tiempo
+      </li>
+      <li>
+        Alta testabilidad de nuestra lógica de negocio gracias a desacoplarnos
+        de la infraestructura
+      </li>
+      <li>Facilitar la colaboración entre distintos equipos de desarrollo</li>
+    </ul>
+  </section>
+</template>
+
+<style scoped>
+.course-content {
+  max-width: 90ch;
+  margin: 0 auto;
+  padding: 0 4rem;
+  font-size: 1.2rem;
+}
+.course-content h3 {
+  font-size: 1.7rem;
+}
+.course-content-title {
+  font-size: 2.7rem;
+}
+.course-content-lead {
+  margin-bottom: 4rem;
+  padding-bottom: 2rem;
+  color: rgba(0, 0, 0, 0.7);
+  font-size: 1.5rem;
+  font-family: var(--ff-primary);
+  line-height: 1.3;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ pages/4-1-modal-position/components/Hero.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="hero">
+    <div class="hero-content">
+      <h1>Cursos para programadores&nbsp;🚀</h1>
+      <p>
+        Aprende de profesionales con experiencia en cursos prácticos y sin
+        rodeos. Mejora tus habilidades, especialízate, y sigue avanzando en tu
+        carrera.
+      </p>
+    </div>
+    <figure class="hero-figure">
+      <img src="~/assets/images/cursos-codelytv-pro.png" alt="" />
+      <figcaption>
+        Cursos de principios SOLID, Domain-Driven Design, Arquitectura
+        Hexagonal, Docker, CQRS, Microservicios, Kubernetes, Testing y mucho
+        más.
+      </figcaption>
+    </figure>
+  </section>
+</template>
+
+<style scoped>
+.hero {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  max-width: var(--max-width);
+  margin: 0 auto;
+  padding: 0 0 2rem;
+  color: #fff;
+}
+.hero > * {
+  margin: 1rem;
+}
+
+.hero-content {
+  flex-basis: 0;
+  flex-grow: 999;
+  min-width: calc(48% - 2rem);
+  padding-right: 2rem;
+}
+.hero-content h1 {
+  font-size: 2.7rem;
+}
+@media (min-width: 32rem) {
+  .hero-content h1 {
+    font-size: 3.2rem;
+  }
+}
+.hero-content p {
+  font-size: 1.5rem;
+}
+
+.hero-figure {
+  flex-basis: 33rem;
+  flex-grow: 1;
+  max-width: 33rem;
+}
+.hero-figure img {
+  max-width: 100%;
+  margin-bottom: 1rem;
+}
+</style>
