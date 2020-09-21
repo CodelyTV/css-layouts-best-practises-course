245a928c4296e9de0b94884a2e34f203eb44c301
Nuria <soriano.nuria@gmail.com>
2020-09-21 10:21:20 +0200

    add lesson 4-3-understanding-z-index start

---
+++ pages/4-3-understanding-z-index/components/Courses.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="block">
+    <section class="courses">
+      <h2 class="courses-title">👌 Cursos disponibles</h2>
+      <Filters />
+      <div class="courses-collection">
+        <Card
+          v-for="course in courses"
+          :key="course.title"
+          :course="course"
+          @click="openModal"
+        />
+      </div>
+      <transition name="fade">
+        <Modal v-if="isOpen" @close="closeModal">
+          <CourseInfo />
+        </Modal>
+      </transition>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.block {
+  background: #eee;
+}
+
+.courses {
+  max-width: var(--max-width);
+  margin: 0 auto;
+  padding: 4rem 1rem;
+}
+.courses-title {
+  font-size: 2rem;
+}
+.courses-collection {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+  gap: 1.5rem;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
+
+<script>
+import Card from './Card'
+import CourseInfo from './CourseInfo'
+import Filters from './Filters'
+import Modal from './Modal'
+import courses from '~/content/courses.json'
+
+export default {
+  components: {
+    Card,
+    CourseInfo,
+    Filters,
+    Modal,
+  },
+  data() {
+    return {
+      isOpen: false,
+      courses,
+    }
+  },
+  methods: {
+    openModal() {
+      this.isOpen = true
+    },
+    closeModal() {
+      this.isOpen = false
+    },
+  },
+}
+</script>
---
+++ pages/4-3-understanding-z-index/components/Filters.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="filters">
+    <ul class="filters-tags">
+      <li v-for="filter in filters" :key="filter">
+        <Checkbox :value="filter" />
+      </li>
+    </ul>
+    <label for="search" class="sr-only">Buscar</label>
+    <input id="search" type="search" placeholder="Buscar…" class="search" />
+  </section>
+</template>
+
+<style scoped>
+.filters {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  max-width: var(--max-width);
+  margin: 2rem auto;
+}
+
+.filters-tags {
+  display: flex;
+  flex-grow: 1;
+  margin: 0 auto 0 -0.5rem;
+  padding: 0.5rem;
+  overflow: auto;
+  column-gap: 0.2rem;
+  list-style: none;
+  mask-image: linear-gradient(to left, transparent 1%, black 13%);
+}
+
+.search {
+  flex-basis: 0;
+  flex-grow: 1;
+  padding: 0.3rem 0.5rem;
+  font-size: 1rem;
+  border: 1px solid transparent;
+}
+.search:focus {
+  border: 1px solid var(--primary-color);
+  outline: 2px solid var(--primary-color);
+}
+</style>
+
+<script>
+import Checkbox from '~/common/Checkbox'
+
+export default {
+  components: {
+    Checkbox,
+  },
+  data() {
+    return {
+      activeFilters: [],
+      filters: [
+        'Arquitectura',
+        'DevOps',
+        'Tooling',
+        'Backend',
+        'JavaScript',
+        'Mobile',
+      ],
+    }
+  },
+}
+</script>
